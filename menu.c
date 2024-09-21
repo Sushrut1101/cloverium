3e7bcc9ce4ba1568850630643a2af14b46455def
anon <166017789+drewdevault@users.noreply.github.com>
2024-09-21 23:03:20 +0200

    Move buttons to correct position and set background

--- menu.c
+++ menu.c
@@ -11,19 +11,38 @@ int main(int argc, char *argv[]) {
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
 
-    // Create a box to hold the image and buttons
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    // Set window size
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 720);
 
-    // Create the image widget
+    // Create a fixed container
+    GtkWidget *fixed = gtk_fixed_new();
+    gtk_container_add(GTK_CONTAINER(window), fixed);
+
+    // Load an image and set it as a background
     GtkWidget *image = gtk_image_new_from_file("/home/anon/.comfy/menubg.png");
+    gtk_fixed_put(GTK_FIXED(fixed), image, 0, 0);
 
-    // Add widgets to the box
-    gtk_box_pack_start(GTK_BOX(vbox), image, TRUE, TRUE, 0);
+    // Create a vertical box to hold the buttons
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_fixed_put(GTK_FIXED(fixed), vbox, 13, 10); // Position the box at the top-left
 
-    // Add the box to the window
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+    // Create the buttons
+    GtkWidget *button1 = gtk_button_new_with_label("Button 1");
+    GtkWidget *button2 = gtk_button_new_with_label("Button 2");
+    GtkWidget *button3 = gtk_button_new_with_label("Button 3");
+    GtkWidget *button4 = gtk_button_new_with_label("Button 4");
+    GtkWidget *button5 = gtk_button_new_with_label("Button 5");
+    GtkWidget *button6 = gtk_button_new_with_label("Button 6");
 
-    // Make the window visible (this will trigger realization and size calculation)
+    // Add the buttons to the vertical box
+    gtk_box_pack_start(GTK_BOX(vbox), button1, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button2, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button3, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button4, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button5, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button6, FALSE, FALSE, 0);
+
+    // Make the window visible
     gtk_widget_show_all(window);
 
     // Get the screen
@@ -33,11 +52,13 @@ int main(int argc, char *argv[]) {
     int screen_width = gdk_screen_get_width(screen);
     int screen_height = gdk_screen_get_height(screen);
 
-    // Get the window's actual height after realization
-    int window_height = gtk_widget_get_allocated_height(window);
-
     // Set the window position to the bottom left corner, adjusted for the 50px margin
-    gtk_window_move(GTK_WINDOW(window), 0, screen_height - window_height - 50);
+    gtk_window_move(GTK_WINDOW(window), 3, screen_height - 640 - 130);
+
+    // Load CSS stylesheet
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path("style.css"), NULL);
+    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     // Start the GTK+ event loop
     gtk_main();
